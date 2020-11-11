4a47a1c4ab554d6108afbcb1a73c7b22d37e5657
Hank McLendon <hank@thebarnroom.com>
2020-11-10 22:59:58 -0500

    Added intro fade in

---
+++ WBI-VR-VRIF/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioClip soundFile;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = soundFile;
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.Play();
+    }
+}
