b5b524c4f45bb195e7343646134e8ce7a6678762
Hank McLendon <hank@thebarnroom.com>
2020-12-08 14:52:03 -0500

    Added rough Scan Exterior activity

---
+++ WBI-VR-VRIF/Assets/Sinthetik/Helpers/Scripts/CameraRaycastDetector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+namespace Sinthetik.Helpers
+{
+    public class CameraRaycastDetector : MonoBehaviour
+    {
+        public Camera camera;
+        public Transform targetPoint;
+        public float holdGazeTimeInSeconds = 2;
+        private float currentGazeTimeInSeconds = 0;
+
+        public UnityEvent hitDetected = new UnityEvent();
+
+        void Update()
+        {
+            Vector3 screenPoint = camera.WorldToViewportPoint(targetPoint.position);
+            if (screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1)
+            {
+                currentGazeTimeInSeconds += Time.deltaTime;
+                if (currentGazeTimeInSeconds >= holdGazeTimeInSeconds)
+                {
+                    currentGazeTimeInSeconds = 0;
+                    hitDetected?.Invoke();
+
+                }
+            }
+            else
+            {
+                currentGazeTimeInSeconds = 0;
+            }
+        }
+    }
+}
